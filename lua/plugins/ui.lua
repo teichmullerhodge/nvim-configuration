c1bb8f0329d76a77b727396a4750b9a7b623ae1a
teichmullerhodge <matheussilvatech@gmail.com>
2025-10-25 22:02:47 -0300

    Modifications to preserve files in .gitignore.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,7 +3,10 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("nvim-tree").setup()
+      require("nvim-tree").setup({
+        git = { ignore = false },
+        filters = { dotfiles = false },
+      })
     end,
   },
   {
@@ -15,11 +18,18 @@ return {
       })
     end,
   },
-    {
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("telescope").setup({})
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+            no_ignore = true,
+          },
+        },
+      })
     end,
   },
 }
