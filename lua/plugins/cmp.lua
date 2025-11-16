6ce0cb5681382b37d723f805fc46678f65efbf40
teichmullerhodge <matheussilvatech@gmail.com>
2025-11-16 07:55:40 -0300

    Removed all the tab.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -30,28 +30,7 @@ return {
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-        -- Tab e Shift-Tab corretos
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif vim.fn.col('.') > 1 and vim.fn.getline('.'):sub(vim.fn.col('.')-1, vim.fn.col('.')-1):match("%S") then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+       
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
